1eec2e69559075f68950bd1431fa571ea0c4fefa
Santiago Carreira <stgocarreira@gmail.com>
2022-04-21 23:15:57 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponenteNuevoComponent } from './componente-nuevo/componente-nuevo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteNuevoComponent
   ],
   imports: [
     BrowserModule,
