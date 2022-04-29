9ba0a324e1689578d01385a2c6c63f6c9786a8ee
Santiago Carreira <stgocarreira@gmail.com>
2022-04-28 23:09:56 -0300

    porfolio modificado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponenteNuevoComponent } from './componente-nuevo/componente-nuevo.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { InteresesComponent } from './componentes/intereses/intereses.component';
+import { CertificacionesComponent } from './componentes/certificaciones/certificaciones.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import {HttpClientModule}  from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponenteNuevoComponent
+    ComponenteNuevoComponent,
+    EncabezadoComponent,
+    SobremiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    InteresesComponent,
+    CertificacionesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
